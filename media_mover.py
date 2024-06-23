876137ba36bfe073d08a1acc5f6c0db173c4549b
cocciasecca <75637780+cocciasecca@users.noreply.github.com>
2024-06-23 18:21:38 +0200

    Update media_mover.py
    Integrated media_lister.py in media_mover.py

--- media_mover.py
+++ media_mover.py
@@ -1,4 +1,18 @@
 import os
+import re
+
+def extract_media(chat_file, media_list_file):
+    media_list = []
+
+    with open(chat_file, 'r', encoding='utf-8') as file:
+        for line in file:
+            media_match = re.search(r'(\(\w+ (?:omessi|file non presente)\))|(\w+(?:-\w+)*\.(?:opus|webp|jpg|mp4))', line)
+            if media_match:
+                media = media_match.group()
+                media_list.append(media)
+
+    with open(media_list_file, 'w', encoding='utf-8') as file:
+        file.write('\n'.join(media_list))
 
 def move_media(media_list_file, media_dir):
     with open(media_list_file, 'r', encoding='utf-8') as file:
@@ -15,7 +29,15 @@ def move_media(media_list_file, media_dir):
         else:
             print(f"File '{media}' not found.")
 
+chat_file = 'chat.txt'
 media_list_file = 'media_list.txt'
-media_dir = 'media'
+print("Created media list txt file.")
 
+extract_media(chat_file, media_list_file)
+
+media_dir = 'media'
 move_media(media_list_file, media_dir)
+
+respond = input("Do you want to keep the media_list.txt file, with all the media listed?(Y/N): ").strip().lower()
+if respond == 'n':
+    os.remove("media_list.txt")
