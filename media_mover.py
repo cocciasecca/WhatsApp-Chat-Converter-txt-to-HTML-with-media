5a9f30e3bf02a9a7a41d2061d1847ba4386126bc
cocciasecca <75637780+cocciasecca@users.noreply.github.com>
2024-06-26 00:27:14 +0200

    Update media_mover.py
    added csv and vcf support

--- media_mover.py
+++ media_mover.py
@@ -6,7 +6,7 @@ def extract_media(chat_file, media_list_file):
 
     with open(chat_file, 'r', encoding='utf-8') as file:
         for line in file:
-            media_match = re.search(r'(\(\w+ (?:omessi|file non presente)\))|(\w+(?:-\w+)*\.(?:opus|webp|jpg|mp4))', line)
+            media_match = re.search(r'(\(\w+ (?:omessi|file non presente)\))|(\w+(?:-\w+)*\.(?:opus|webp|jpg|mp4|vcf|csv))', line)
             if media_match:
                 media = media_match.group()
                 media_list.append(media)
